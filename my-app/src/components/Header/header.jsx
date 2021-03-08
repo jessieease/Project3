f332b4157f15fb19077bff63a8326fc29f124087
BO WU <bwuu0013@student.monash.edu>
2021-03-08 21:17:25 +1100

    feat(header): start to build up a project

---
+++ my-app/src/components/Header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Layout = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Header = () => {
+    <Layout>
+        header
+    </Layout>
+}
+export default Header;
